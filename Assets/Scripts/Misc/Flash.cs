b24020e170b751ee1db697de3f50c5945ac7abe3
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-02-26 12:26:59 +0700

    Initial commit

---
+++ Assets/Scripts/Misc/Flash.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class Flash : MonoBehaviour
+{
+    [SerializeField] private Material whiteFlashMat;
+    [SerializeField] private float restoreDefaultMatTime = .2f;
+
+    private Material defaultMat;
+    private SpriteRenderer spriteRenderer;
+    private EnemyHealth enemyHealth;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultMat = spriteRenderer.material;
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    public IEnumerator FlashRoutine()
+    {
+        spriteRenderer.material = whiteFlashMat;
+        yield return new WaitForSeconds(restoreDefaultMatTime);
+        spriteRenderer.material = defaultMat;
+        enemyHealth.DetectDeath();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+    public static PlayerController Instance;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControl;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    private bool facingLeft;
+
+    private void Awake()
+    {
+        Instance = this;
+        playerControl = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControl.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControl.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+            FacingLeft = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+            FacingLeft = false;
+        }
+    }
+}
