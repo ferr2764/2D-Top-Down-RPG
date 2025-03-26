f70d19904c717a94329aad83b97e4cb5442bf56c
Dang Minh Duc <ducdm160804@gmail.com>
2025-03-26 14:48:09 +0700

    scene

---
+++ Assets/Scripts/Enemies/EnemyChase.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    public Transform player; // Nhân vật người chơi
+    public float speed = 3f;
+    public float detectRange = 5f;
+    public Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveDir;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        if (distance < detectRange)
+        {
+            // Tính toán hướng di chuyển
+            moveDir = (player.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+            // Cập nhật flip dựa trên hướng di chuyển
+            UpdateAnimation();
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        // Lật sprite nếu di chuyển trái/phải
+        if (moveDir.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveDir.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        // Chọn animation theo hướng di chuyển chính
+        animator.SetFloat("MoveX", moveDir.x);
+        animator.SetFloat("MoveY", moveDir.y);
+    }
+}
---
+++ Assets/Scripts/Enemies/PlayerAnimationController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public Animator animator;
+
+    public RuntimeAnimatorController walkRight;
+    public RuntimeAnimatorController walkLeft;
+    public RuntimeAnimatorController walkFront;
+    public RuntimeAnimatorController walkBack;
+
+    private Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Kiểm tra hướng di chuyển và đổi Animator Controller
+        if (movement.x > 0)
+        {
+            animator.runtimeAnimatorController = walkRight;
+        }
+        else if (movement.x < 0)
+        {
+            animator.runtimeAnimatorController = walkLeft;
+        }
+        else if (movement.y > 0)
+        {
+            animator.runtimeAnimatorController = walkBack;
+        }
+        else if (movement.y < 0)
+        {
+            animator.runtimeAnimatorController = walkFront;
+        }
+    }
+}
