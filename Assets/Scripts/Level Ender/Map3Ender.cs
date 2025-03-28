7802c1fa5210adcd8609e6141f6399bedc42453e
Dang Minh Duc <ducdm160804@gmail.com>
2025-03-28 16:32:14 +0700

    screen

--- Assets/Scripts/Level Ender/Map3Ender.cs
+++ Assets/Scripts/Level Ender/Map3Ender.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class Map2Ender : MapEnder
+public class Map3Ender : MapEnder
 {
     public override void Action()
     {
         //Open slot 2
         var activeInv = ActiveInventory.Instance;
         //Inventory 2 (game object)
-        var inventory2 = activeInv.transform.GetChild(1).gameObject;
+        var inventory2 = activeInv.transform.GetChild(2).gameObject;
         var slot = inventory2.GetComponent<InventorySlot>();
         slot.enabled = true;
         //Item (game object)
