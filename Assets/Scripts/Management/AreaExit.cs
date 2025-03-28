ed9429f3e1b5276adce628d6b01775cdaeed2a67
ngoc-main <dangngoc4332@gmail.com>
2025-03-28 22:05:40 +0700

    Must kill all to get to next level

--- Assets/Scripts/Management/AreaExit.cs
+++ Assets/Scripts/Management/AreaExit.cs
@@ -8,10 +8,15 @@ public class AreaExit : MonoBehaviour
     [SerializeField] private string sceneToLoad;
     [SerializeField] private string sceneTransitionName;
     private float waitToLoadTime = 1f;
+    private bool killedAllEnemies = false;
+    public static bool GotItem { get; set; } = false;
+    [SerializeField] bool suppressRequirement = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>())
         {
+            killedAllEnemies = false;
+            GotItem = false;
             PlayerHealth.Instance.HealAmount(10);
             SceneManagement.Instance.SceneTransitionName = sceneTransitionName;
             UIFade.Instance.Fade(1);
@@ -27,5 +32,18 @@ public class AreaExit : MonoBehaviour
             yield return null;
         }
         SceneManager.LoadScene(sceneToLoad);
+    }
+    public void Update()
+    {
+        killedAllEnemies = CheckAllKilled();
+        if(suppressRequirement ||killedAllEnemies && GotItem)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    private bool CheckAllKilled()
+    {
+        return GameObject.FindWithTag("Enemy") == null;
     }
 }
